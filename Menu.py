5535327b6d9095151517c72e7f29744abe3515a0
DrFriz <102406230+DrFriz@users.noreply.github.com>
2022-03-26 03:15:09 -0500

    Add files via upload

--- Menu.py
+++ Menu.py
@@ -1,6 +1,7 @@
 from cProfile import label
 from tkinter import *
 from turtle import title
+from tkinter import ttk
 
 
 #  Import tkinter as tk
@@ -8,6 +9,8 @@ root = Tk()
 
 #  Setting Menu Settings
 root.geometry('800x400')
+root.resizable(FALSE, FALSE)
+root.attributes("-alpha", 0.99)
 root.configure(bg='#252526')
 
 
@@ -15,15 +18,19 @@ root.option_add('*tearOff', FALSE)
 root.title('Radio App')
 
 #  Radio Menu Open Def
-def open():
+def RadioMenu():
     top = Toplevel()
+    top.title('Radio Menu')
     top.geometry('800x400')
+    top.resizable(FALSE, FALSE)
     top.configure(bg='#252526')
-    
 
-
-
-
+def DrawingMenu():
+    top1 = Toplevel()
+    top1.title('Drawing Menu')
+    top1.geometry('800x400')
+    top1.resizable(FALSE, FALSE)
+    top1.configure(bg='')
 
 
 
@@ -35,9 +42,9 @@ m = Menu(root)
 m_edit = Menu(m)
 m.add_cascade(menu=m_edit, label="Options")
 root['menu'] = m
-m_edit.add_command(label="Radio Stations", command=open)
+m_edit.add_command(label="Radio Stations", command=RadioMenu)
+m_edit.add_command(label="Drawing Window", command=DrawingMenu)
 m_edit.add_command(label="Exit", command=quit)
-m_edit.add_command(label="New Window")
 
 
 
