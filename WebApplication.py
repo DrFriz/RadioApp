1ee3bd3639843bb7d7625d3d6b8926cfe0e432a5
DrFriz <102406230+DrFriz@users.noreply.github.com>
2022-04-06 10:07:46 -0500

    Update WebApplication.py

--- WebApplication.py
+++ WebApplication.py
@@ -1,70 +1,68 @@
-import time
-import webbrowser
-import tkinter as Tk
 from tkinter import *
-import requests
+from pytest import skip
 import webview
 
 
 root = Tk()
 
-
-# web stuff
-def webv():
-    webview.create_window(title='YouTube', url='https://www.youtube.com/')
-    webview.start()
-    
-
-
-
-# Gui stuff
 root.geometry('800x400')
-root.resizable(FALSE, FALSE)
 root.configure(bg='#252526')
 root.option_add('*tearOff', FALSE)
-root.title('WebApplication')
+root.title('WebApps - Tkinter')
+root.resizable(FALSE, FALSE)
 
+
+      
+def YouTube():
+    webview.create_window(title='YouTube', url='https://www.YouTube.com/')
+    webview.start()
+
+def Twitch():
+    webview.create_window(title='YouTube', url='https://www.Twitch.com/')
+    webview.start()
+
+def Steam():
+    webview.create_window(title='YouTube', url='https://store.steampowered.com/')
+    webview.start()
+
+def Racer():
+    webview.create_window(title='YouTube', url='https://www.Racer.com/')
+    webview.start()
+
+
+
+#  Drop Down Radio Menu
 m = Menu(root)
 m_edit = Menu(m)
 m.add_cascade(menu=m_edit, label="Web-Servers")
 root['menu'] = m
-m_edit.add_command(label="Youtube", command=webv)
-m_edit.add_command(label="Twitch", command=print('http://www.twitch.com'))
-m_edit.add_command(label="Steam", command=print('http://www.Steam.com'))
-m_edit.add_command(label="Radio-Stations", command=print('http://streema.com/ , https://www.iheart.com/'))
+m_edit.add_command(label="Youtube", command= YouTube)
+m_edit.add_command(label="Twitch", command= Twitch)
+m_edit.add_command(label="Steam", command= Steam)
+m_edit.add_command(label="R-A-C-E-R-S", command= Racer)
 m_edit.add_command(label="Exit", command=quit)
+
+
+
+
+
+
+#def YT():
+    #top = Toplevel()
+    #top.geometry('800x400')
+    #top.configure(bg='#252526')
+    #top.option_add('*tearOff', FALSE)
+    #top.title('YouTube - Tkinter')
+    #top.resizable(FALSE, FALSE)
+    #print
+
 root.mainloop()
 
 
-# URL's to Websites
-
-#url = input('http://streema.com/')
-#url1 = input('Webpage to grab source from: ')
-#url2 = input('Webpage to grab source from: ')
-#url3 = input('Webpage to grab source from: ')
-#url4 = input('Webpage to grab source from: ')
-#url5 = input('Webpage to grab source from: ')
-
-
-#html_output_name = input('Name for html file: ')
-
-#req = requests.get(url, 'html.parser')
-
-#with open(html_output_name, 'w') as f:
-#    f.write(req.text)
-#    f.close()
-
-#def Main():
-#    global Main
-#    global top
-#    top = Toplevel()
-#    top.geometry('800x400')
-#    top.resizable(FALSE, FALSE)
-#    button = Button(root, text="Play", command=Main).pack()
-#root.mainloop()
 
 
 
-# Set to Vm
-#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
-#venv\scripts\activate
+
+
+#  Radio Websites
+#  https://streema.com/ , https://www.iheart.com/
